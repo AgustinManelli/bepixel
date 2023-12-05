ff630d56baed11589111a670d4e07ed6dae33a41
Agustin Manelli <agustinmanelli@hotmail.com>
2023-12-05 13:30:30 -0300

    add no filter comment

--- src/components/toolspage/ToolsBody.js
+++ src/components/toolspage/ToolsBody.js
@@ -1,22 +1,28 @@
 import '../../stylesheets/toolspage/ToolsBody.css';
 import { motion } from 'framer-motion';
 import ToolsItems from './ToolsItems';
+import { useTranslation } from 'react-i18next';
 
 function ToolsBody({ items, lastIndex, firstIndex }) {
+	const [t] = useTranslation('global');
 	return (
 		<section className='itemsBody' id='itemsBody'>
-			<div className='itemsBodyContainer'>
-				{items
-					.map(items => (
-						<motion.div
-							key={items.id}
-							whileHover={{ scale: 1.01 }}
-							transition={{ type: 'spring', stiffness: 800, damping: 20 }}>
-							<ToolsItems items={items} />
-						</motion.div>
-					))
-					.slice(firstIndex, lastIndex)}
-			</div>
+			{items != 0 ? (
+				<div className='itemsBodyContainer'>
+					{items
+						.map(items => (
+							<motion.div
+								key={items.id}
+								whileHover={{ scale: 1.01 }}
+								transition={{ type: 'spring', stiffness: 800, damping: 20 }}>
+								<ToolsItems items={items} />
+							</motion.div>
+						))
+						.slice(firstIndex, lastIndex)}
+				</div>
+			) : (
+				<p>{t('toolsbody.ups')}</p>
+			)}
 		</section>
 	);
 }
